6627b95d7171036274fc3b55f77adb8658c6d648
moreih29 <moreih29@gmail.com>
2025-04-23 21:41:26 +0900

    fix: hotkey를 qt로 구현

--- src/pyside6_manager/qml/FluentUI/plugins/FluHotkey.py
+++ src/pyside6_manager/qml/FluentUI/plugins/FluHotkey.py
@@ -1,8 +1,7 @@
-import keyboard
+from PySide6.QtCore import Signal, QObject, Property, Qt
+from PySide6.QtGui import QKeySequence, QGuiApplication, QShortcut
 
-from PySide6.QtCore import Signal, QObject, Property
-
-# pyright: reportRedeclaration=none
+# pyright: reportRedeclaration=none, reportAttributeAccessIssue=none
 
 
 # noinspection PyPep8Naming
@@ -12,24 +11,61 @@ class FluHotkey(QObject):
     isRegisteredChanged = Signal()
     activated = Signal()
 
-    def hotkeyCallback(self):
-        self.activated.emit()
-
     def __init__(self):
         QObject.__init__(self)
         self._sequence: str = ""
         self._name: str = ""
         self._isRegistered: bool = False
+        self._shortcut: QShortcut | None = None
+
+        # QApplication 인스턴스 확인 및 취득
+        self._app = QGuiApplication.instance()
 
         def handleSequenceChanged():
-            # keyboard.remove_hotkey(self.hotkeyCallback)
+            # 이전 단축키 제거
+            if self._shortcut:
+                self._shortcut.setEnabled(False)
+                self._shortcut.deleteLater()
+                self._shortcut = None
+
             try:
-                keyboard.add_hotkey(self._sequence, self.hotkeyCallback)
-                self.isRegistered = True  # pyright: ignore[reportAttributeAccessIssue]
-            except RuntimeError:
-                self.isRegistered = False  # pyright: ignore[reportAttributeAccessIssue]
+                if not self._sequence:
+                    self.isRegistered = False
+                    return
+
+                # Qt 단축키 형식으로 변환
+                qt_sequence = self._convert_to_qt_sequence(self._sequence)
+
+                # 전역 단축키 등록
+                if self._app:
+                    self._shortcut = QShortcut(QKeySequence(qt_sequence), self._app)
+                    self._shortcut.activated.connect(self.hotkeyCallback)
+                    self._shortcut.setContext(Qt.ShortcutContext.ApplicationShortcut)
+                    self.isRegistered = True
+            except Exception:
+                self.isRegistered = False
 
         self.sequenceChanged.connect(lambda: handleSequenceChanged())
+
+    def _convert_to_qt_sequence(self, sequence: str):
+        """키보드 라이브러리 형식의 단축키를 Qt 형식으로 변환합니다."""
+        # macOS에서는 cmd를 Meta로 처리
+        sequence = sequence.replace("cmd", "Meta")
+        sequence = sequence.replace("command", "Meta")
+        sequence = sequence.replace("alt", "Alt")
+        sequence = sequence.replace("ctrl", "Ctrl")
+        sequence = sequence.replace("control", "Ctrl")
+        sequence = sequence.replace("shift", "Shift")
+
+        # + 기호 주변에 공백 제거
+        sequence = sequence.replace(" + ", "+")
+        sequence = sequence.replace(" +", "+")
+        sequence = sequence.replace("+ ", "+")
+
+        return sequence
+
+    def hotkeyCallback(self):
+        self.activated.emit()
 
     @Property(bool, notify=isRegisteredChanged)
     def isRegistered(self):
@@ -37,8 +73,9 @@ class FluHotkey(QObject):
 
     @isRegistered.setter
     def isRegistered(self, value: bool):
-        self._isRegistered = value
-        self.isRegisteredChanged.emit()
+        if self._isRegistered != value:
+            self._isRegistered = value
+            self.isRegisteredChanged.emit()
 
     @Property(str, notify=nameChanged)
     def name(self):
@@ -46,8 +83,9 @@ class FluHotkey(QObject):
 
     @name.setter
     def name(self, value: str):
-        self._name = value
-        self.nameChanged.emit()
+        if self._name != value:
+            self._name = value
+            self.nameChanged.emit()
 
     @Property(str, notify=sequenceChanged)
     def sequence(self):
@@ -55,5 +93,6 @@ class FluHotkey(QObject):
 
     @sequence.setter
     def sequence(self, value: str):
-        self._sequence = value
-        self.sequenceChanged.emit()
+        if self._sequence != value:
+            self._sequence = value
+            self.sequenceChanged.emit()
